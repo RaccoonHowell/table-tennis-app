67bc500a21b04c7ff1176e488adb1e4a9d910226
Rachel <rac777blue@hotmail.com>
2020-03-30 16:28:35 +0100

    made a components folder and created Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => (
-  <Header />
+	<>
+		<Header />
+		<Footer />
+	</>
 );
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+        return (
+            <p className="copyright">&copy; Rachel Howell 2020</p>
+        )
+    }
+
+export default Footer;
