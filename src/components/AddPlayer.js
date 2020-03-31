2d651a250101775dee7a95e21db0e55b4c28c699
Rachel <rac777blue@hotmail.com>
2020-03-31 21:51:42 +0100

    added event listeners and handlers to AddPlayer component so that state updates with any input changes and each time a player is added using the submit button it updates the players array

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Addplayer = () => {
-    return (
-        <>
-            <input type="text" />
+export class AddPlayer extends Component {
+    constructor(props) {
+        super(props);
 
-            <button>Add player</button>
-        </>
-    )
+        this.state = {
+            userInput: '',
+            players: [],
+        }
+
+        this.handleUserInput = this.handleUserInput.bind(this);
+        this.handleSubmit= this.handleSubmit.bind(this);
+    }
+
+    handleUserInput(e) {
+        this.setState({
+            userInput: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({
+            userInput: '',
+            players: [...this.state.players, this.state.userInput],
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" onChange={this.handleUserInput} value={this.state.userInput}/>
+
+                <button>Submit</button>
+
+                <ul>Players
+                    { this.state.players.map((player, i) => (
+                        <li key={ i }>{ player }</li>
+                    )) }
+                </ul>
+            </form>
+        );
+    }
 }
 
-export default Addplayer;
+export default AddPlayer;
