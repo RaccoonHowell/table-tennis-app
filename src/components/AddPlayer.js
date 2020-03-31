f940a3cdb7757c08e446bda28eec42d0f2789b4a
Rachel <rac777blue@hotmail.com>
2020-03-31 23:02:33 +0100

    added a player1 and player 2 to initial state, created basic handlePair function and added it to the generate button's onClick but need to make it so that the generate button only returns player names if they are not the same

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -7,11 +7,14 @@ export class AddPlayer extends Component {
         this.state = {
             userInput: '',
             players: [],
+            player1: '',
+            player2: '',
         }
 
         this.handleUserInput = this.handleUserInput.bind(this);
-        this.handleSubmit= this.handleSubmit.bind(this);
-        this.handleReset= this.handleReset.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.handlePair = this.handlePair.bind(this);
     }
 
     // updates state when user types in input field
@@ -36,6 +39,18 @@ export class AddPlayer extends Component {
         this.setState({
             userInput: '',
             players: [],
+            player1: '',
+            player2: '',
+        })
+    }
+
+    // selects player1 and player2 from the players array by setting the players index using Math.random
+    handlePair() {
+        const { players } = this.state;
+
+        this.setState({
+            player1: players[Math.floor(Math.random() * players.length)],
+            player2: players[Math.floor(Math.random() * players.length)]
         })
     }
 
@@ -54,7 +69,9 @@ export class AddPlayer extends Component {
                     </ul>
                 </form>
 
-                <button>Generate pair</button>
+                <button onClick={ this.handlePair }>Generate pair</button>
+
+                <p>{ this.state.player1 } v { this.state.player2 }</p>
 
                 <button onClick={ this.handleReset }>Reset</button>
             </>
