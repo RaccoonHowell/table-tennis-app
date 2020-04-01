8be6c60322d1b55ae85f693a14b03dcf6ea6cd55
Rachel <rac777blue@hotmail.com>
2020-04-01 15:22:43 +0100

    added a shuffled array and started trying to split the array into 2

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -7,14 +7,16 @@ export class AddPlayer extends Component {
         this.state = {
             userInput: '',
             players: [],
-            player1: '',
-            player2: '',
+            playersShuffled: [],
+            column1: [],
+            column2: [],
         }
 
         this.handleUserInput = this.handleUserInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleReset = this.handleReset.bind(this);
-        this.handlePair = this.handlePair.bind(this);
+        this.handlePairs = this.handlePairs.bind(this);
+        // this.handleShuffle = this.handleShuffle.bind(this);
     }
 
     // updates state when user types in input field
@@ -39,25 +41,35 @@ export class AddPlayer extends Component {
         this.setState({
             userInput: '',
             players: [],
-            player1: '',
-            player2: '',
+            playersShuffled: [],
+            column1: [],
+            column2: [],
         })
     }
+
+    // handleShuffle(array) {
+    //     array.sort(() => Math.random() - 0.5);
+    // }
 
     // selects player1 and player2 from the players array by setting the players index using Math.random
-    handlePair() {
+    handlePairs() {
         const { players } = this.state;
 
+        const half_length = Math.ceil(players.length / 2);    
+
         this.setState({
-            player1: players[Math.floor(Math.random() * players.length)],
-            player2: players[Math.floor(Math.random() * players.length)]
+            playersShuffled: players.sort(() => Math.random() - 0.5),
+            // playersShuffled: this.handleShuffle(players),
+            column1: this.playersShuffled.splice(0, half_length),
+            column2: this.playersShuffled.splice(half_length, this.playersShuffled.length),
         })
     }
-
+    
     render() {
         return (
             <>
                 <form onSubmit={this.handleSubmit}>
+                    <label>Add an even number of players</label>
                     <input placeholder="Enter player names here" type="text" onChange={this.handleUserInput} value={this.state.userInput}/>
 
                     <button>Add player</button>
@@ -69,9 +81,9 @@ export class AddPlayer extends Component {
                     </ul>
                 </form>
 
-                <button onClick={ this.handlePair }>Generate pair</button>
+                <button onClick={ this.handlePairs }>Generate pairs</button>
 
-                <p>{ this.state.player1 } v { this.state.player2 }</p>
+                <p>{this.state.playersShuffled}</p>
 
                 <button onClick={ this.handleReset }>Reset</button>
             </>
