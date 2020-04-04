a372ff2571bb27d42373124dd8422d2f9350de97
Rachel <rac777blue@hotmail.com>
2020-04-04 21:04:35 +0100

    Updates

---
+++ precache-manifest.6c0209e1ec2aa06370e469d7c9bc9433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b22e3738123224d6c1392ce20b8f72",
+    "url": "/raccoonhowell.github.io/table-tennis-app/index.html"
+  },
+  {
+    "revision": "95f19a86abcf24986049",
+    "url": "/raccoonhowell.github.io/table-tennis-app/static/css/main.ab8e4aec.chunk.css"
+  },
+  {
+    "revision": "9f8d2b10144dce6c8ad6",
+    "url": "/raccoonhowell.github.io/table-tennis-app/static/js/2.175e5c2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/raccoonhowell.github.io/table-tennis-app/static/js/2.175e5c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f19a86abcf24986049",
+    "url": "/raccoonhowell.github.io/table-tennis-app/static/js/main.a76d7f2f.chunk.js"
+  },
+  {
+    "revision": "bbe1bc646034016edc79",
+    "url": "/raccoonhowell.github.io/table-tennis-app/static/js/runtime-main.65c0de11.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/raccoonhowell.github.io/table-tennis-app/precache-manifest.6c0209e1ec2aa06370e469d7c9bc9433.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raccoonhowell.github.io/table-tennis-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
